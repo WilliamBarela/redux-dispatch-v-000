32411e460df86776f046cff949b144fac6e07543
William Barela <william.barela.dev@gmail.com>
2020-08-24 13:40:44 -0500

    Done.

--- js/reducer.js
+++ js/reducer.js
@@ -3,7 +3,7 @@ let state = {count: 0};
 function changeState(state, action){
     switch (action.type) {
       case 'INCREASE_COUNT':
-        return {count: state.count + 1}
+        return {count: state.count + 2}
       default:
         return state;
     }
